9862c0a03f28243b59572dcaa57b27d41b6aff6f
Cauê Santos <caah.sfc@live.com>
2020-06-27 13:18:33 -0300

    Implementando filtro de autenticação

---
+++ src/main/java/br/com/cauezito/filter/FilterAuth.java
@@ -0,0 +1,47 @@
+package br.com.cauezito.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import br.com.cauezito.entity.Person;
+import br.com.cauezito.util.JPAUtil;
+
+@WebFilter(urlPatterns = {"/*"})
+public class FilterAuth implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		JPAUtil.getEntityManager();		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		
+		Person person = (Person) session.getAttribute("personOn");
+		String url = req.getServletPath();
+		if(!url.equalsIgnoreCase("index.xhtml") && person == null){
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/login.xhtml");
+			dispatcher.forward(request, response);
+			return;
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {}
+
+}
